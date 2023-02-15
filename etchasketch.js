6e53d517c75340b8131fd045a08a4ad2d2c5baa6
copaiement <cole.paiement@gmail.com>
2023-02-15 10:53:48 -0800

    Update etchasketch with working mode. Color changes and click changes not supported yet.

--- etchasketch.js
+++ etchasketch.js
@@ -1,21 +1,35 @@
 // JS for Etch a Sketch Webpage
 
+//setup default vars
+let fillColor;
+let colorStyle;
+let penStyle;
+
+
+//set up initial board
 function initialize() {
     const initialPixel = 16;
     for (let i = 1; i <= (initialPixel * initialPixel); i ++) {
         addPixel();
     }
+    fillColor = "black";
+    colorStyle = "black";
+    penStyle = "mouseover";
 }
 
+//add pixel to board
 function addPixel() {
     const artboard = document.querySelector(".artboard");
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
+    //set bg color white
+    pixel.style.backgroundColor = "white";
     //add pixel into artboard
     artboard.appendChild(pixel);
-
 }
+
 // ADD REJECTION FOR DECIMALS
+//resize board
 function resize() {
     //get new size
     let getValue = document.getElementById("value");
@@ -47,12 +61,53 @@ function resize() {
     artboard.style.gridTemplateColumns = `repeat(${resizeNum}, minmax(0, 1fr))`;
 }
 
+//clear color from board
 function erase() {
-    
-    //was testing with this. it is probably broken
-    const artboard = document.querySelector(".artboard");
-    //const pixels = document.querySelectorAll(".pixel");
-    artboard.style.cssText = "grid-template-columns: repeat(12, minmax(0, 1fr))"
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel => {
+        pixel.style.backgroundColor = 'white'
+    });
+}
+
+//decide which fill style to use
+function setColorStyle(colorStyle) {
+    //Options: pick, random, shade
+    return colorStyle;
+}
+
+//set pen style (pull value from HTML: btn)
+function setPenStyle(penStyle) {
+    //options: click, mouseover
+    console.log(penStyle);
+    return penStyle; 
+}
+
+function randomRgb() {
+    //choose random r, b, and g vals from 0 to 255
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let rgb = `rgb(${r}, ${g}, ${b})`;
+    return rgb;
 }
 
 initialize();
+
+
+const pixels = document.querySelectorAll(".pixel");
+//look for mouse interaction to change color
+pixels.forEach(pixel => {
+
+    pixel.addEventListener(`${penStyle}`, () => {
+        
+        fillColor = randomRgb();
+        console.log(fillColor);
+        console.log(penStyle);
+        
+        //change color of pixel
+        pixel.style.backgroundColor = `${fillColor}`;
+
+
+    });
+});
+
