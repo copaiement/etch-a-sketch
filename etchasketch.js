04cb6b9e2598c8a0b429f5269b2b54510c35cfbc
copaiement <cole.paiement@gmail.com>
2023-02-15 22:25:53 -0800

    Update all functions to work properly. Everything is working including shader function. Next step (optionally) is to make everything a bit prettier (css and HTML).

--- etchasketch.js
+++ etchasketch.js
@@ -1,14 +1,22 @@
 // JS for Etch a Sketch Webpage
 
 //setup default vars
-let fillColor = "black";
-let colorStyle = "black";
+let fillColor;
+let colorStyle;
 let penStyle;
+
+initialize();
+
+//set color picker event listener
+const colorPicker = document.getElementById("select-color");
+colorPicker.addEventListener("change", setColor);
 
 //set up initial board
 function initialize() {
     const initialPixel = 16;
     penStyle = "mouseover";
+    fillColor = "rgb(0, 0, 0)";
+    colorStyle = "solid";
     for (let i = 1; i <= (initialPixel * initialPixel); i ++) {
         addPixel();
     }
@@ -21,7 +29,7 @@ function addPixel() {
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
     //set bg color white
-    pixel.style.backgroundColor = "white";
+    pixel.style.backgroundColor = "rgb(255, 255, 255)";
     //add pixel into artboard
     artboard.appendChild(pixel);
 }
@@ -43,13 +51,20 @@ function removeListeners(penStyle) {
 }
 
 function draw(e) {
-    e.target.style.backgroundColor = "black";
+    if (colorStyle == "solid") {
+        e.target.style.backgroundColor = `${fillColor}`;
+    } else if (colorStyle == "random") {
+        e.target.style.backgroundColor = `${randomRgb()}`;
+    } else {
+        let currentColor = e.target.style.backgroundColor;
+        e.target.style.backgroundColor = `${shade(currentColor)}`;
+    }
 }
 
 //resize board
 function resize() {
     //get new size
-    let getValue = document.getElementById("value");
+    let getValue = document.getElementById("size-value");
     let resizeNum = 0;
     //round decimals down to integers
     getValue.value = Math.floor(getValue.value);
@@ -92,29 +107,53 @@ function eraseAll() {
 }
 
 //decide which fill style to use
-function setColorStyle(newColor) {
+function setColorStyle(newStyle) {
     //Options: pick, random, shade
-    return newColor;
+    colorStyle = newStyle;
 }
 
 //set pen style (pull value from HTML: btn)
 function setPenStyle(newStyle) {
-    //options: click, mouseover
+    //remove old listeners
     removeListeners(penStyle);
     penStyle = newStyle;
-    console.log(penStyle);
+    //add new listeners
     addListeners(penStyle);
-    
 }
 
+//function for random color style
 function randomRgb() {
     //choose random r, b, and g vals from 0 to 255
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     let rgb = `rgb(${r}, ${g}, ${b})`;
+    colorStyle = "random";
     return rgb;
 }
 
-initialize();
-// draw();
+//make squares 10% darker each pass
+function shade(currentColor) {
+    let newColor;
+    currentColor = currentColor.replace(/[^\d,]/g, '').split(',');
+   
+    if (currentColor[0] >= 24 && currentColor[1] >= 24 && currentColor[2] >= 24) {
+        newColor = `rgb(${currentColor[0] - 24}, ${currentColor[1] - 24}, ${currentColor[2] - 24})`;
+        
+    } else {
+        newColor = "rgb(0, 0, 0)";
+    }
+
+    colorStyle = "shade";
+    return newColor;
+}
+
+function setColor(e) {
+    fillColor = e.target.value;
+    colorStyle = "solid";
+}
+
+//
+
+
+
